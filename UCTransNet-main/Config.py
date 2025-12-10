8b3e614ab49b66418c3097b5b4f669962f3898c8
Sardine2142 <98326930+Sardine2142@users.noreply.github.com>
2025-12-10 11:05:26 +0900

    Refactor Config.py for task and model updates
    Updated configuration settings for task selection and model name.

--- UCTransNet-main/Config.py
+++ UCTransNet-main/Config.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2021/6/19 2:44 下午
-# @Author  : Haonan Wang
-# @File    : Config.py
-# @Software: PyCharm
 import os
 import torch
 import time
@@ -17,35 +12,52 @@ seed = 666
 os.environ['PYTHONHASHSEED'] = str(seed)
 
 cosineLR = True # whether use cosineLR or not
-n_channels = 3
-n_labels = 1
 epochs = 2000
-img_size = 224
 print_frequency = 1
 save_frequency = 5000
 vis_frequency = 10
 early_stopping_patience = 50
 
 pretrain = False
-task_name = 'MoNuSeg' # GlaS MoNuSeg
-# task_name = 'GlaS'
+
+# ============= TASK 선택 =============
+task_name = 'MoNuSeg'  # 의료 영상 - 세포핵 분할
+# task_name = 'MLD'      # Massachusetts Roads Dataset
+# task_name = 'GlaS'       # Gland Segmentation Dataset
+# =====================================
+
 learning_rate = 1e-3
 batch_size = 4
 
-
 # model_name = 'UCTransNet'
-model_name = 'UCTransNet_pretrain'
+model_name = 'UNet'
+
+# Task별 설정
+if task_name == 'MoNuSeg':
+    n_channels = 3
+    n_labels = 1
+    img_size = 224
+elif task_name == 'MLD':
+    n_channels = 3      # RGB 이미지
+    n_labels = 1        # 이진 분할 (도로 vs 배경)
+    img_size = 224      # 원본 1500x1500을 224로 리사이즈
+elif task_name == 'GlaS':
+    n_channels = 3      # RGB 병리 이미지 (H&E 염색)
+    n_labels = 1        # 이진 분할 (gland vs background)
+    img_size = 224      # 원본 ~775x522를 224로 리사이즈
+else:
+    raise ValueError(f"Unknown task: {task_name}")
 
 train_dataset = './datasets/'+ task_name+ '/Train_Folder/'
 val_dataset = './datasets/'+ task_name+ '/Val_Folder/'
-test_dataset = './datasets/'+ task_name+ '/Test_Folder/'
+test_dataset = './datasets/'+ task_name+ '/Test_Folder/TestB'
+
 session_name       = 'Test_session' + '_' + time.strftime('%m.%d_%Hh%M')
 save_path          = task_name +'/'+ model_name +'/' + session_name + '/'
 model_path         = save_path + 'models/'
 tensorboard_folder = save_path + 'tensorboard_logs/'
 logger_path        = save_path + session_name + ".log"
 visualize_path     = save_path + 'visualize_val/'
-
 
 ##########################################################################
 # CTrans configs
@@ -65,8 +77,6 @@ def get_CTranS_config():
     config.n_classes = 1
     return config
 
-
-
-
 # used in testing phase, copy the session name in training phase
-test_session = "Test_session_07.03_20h39"
+test_session = "Test_session_12.10_08h35"
+
